38978579ce709a2487c37e81e2c8aa9e321bdcf7
Autumn <autumn.m.peters@gmail.com>
2018-10-10 11:38:12 -0700

    Upload3

--- sw.js
+++ sw.js
@@ -1,35 +1,38 @@
-/*Code adapted from https://github.com/udacity/mws-restaurant-stage-3/pull/3/files */
+var CACHE_NAME = 'apcache2';
+var urlsToCache = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/dbhelper.js',
+  '/js/restaurant_info.js',
+  '/js/idb-test.js',
+  'favicon.ico'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('cachev5').then(cache => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'restaurant.html',
-        'css/styles.css',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg',
-        'img/undefined.jpg'
-      ]);
+self.addEventListener('install', function(event) {
+// Perform install steps
+event.waitUntil(
+  caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
-  )
+);
 });
- self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener('fetch', function(event) {
+event.respondWith(
+  caches.match(event.request)
+    .then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }
+
+  )
+);
 });
